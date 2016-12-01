e7ddc4e83ee0038b10b10875e343225c5788f9f2
hux8 <hux8@rpi.edu>
2016-11-30 19:51:27 -0500

    Add files via upload

--- client/resources/supplemental.js
+++ client/resources/supplemental.js
@@ -2,3 +2,9 @@ document.getElementById('addForm').onsubmit = function(event) {
 	event.preventDefault();
 	window.location = "congradulations.html";
 }
+
+function dele(element) {
+  if (element.value == "Please enter your answer"){
+    element.value = "";
+  }
+}
