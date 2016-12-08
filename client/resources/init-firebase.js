d441b71dea02cef6d8dc71c955d84e38cf458cbe
Raz Reed <raz@shipstone.com>
2016-12-08 14:36:13 -0500

    Add files via upload

---
+++ client/resources/init-firebase.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBubD5k6hXA_-9ja_FeNGt-1G9804z3M4U",
+	authDomain: "itws-term-project.firebaseapp.com",
+	databaseURL: "https://itws-term-project.firebaseio.com",
+	storageBucket: "itws-term-project.appspot.com",
+	messagingSenderId: "934123867446"
+};
+firebase.initializeApp(config);
--- client/resources/login.js
+++ client/resources/login.js
@@ -1,30 +1,42 @@
-document.getElementById('addFormApp').onsubmit = function(event) {
-	event.preventDefault();
-	window.location = "appProfile.html";
-}
-document.getElementById('addFormCom').onsubmit = function(event) {
-	event.preventDefault();
-	window.location = "companyProfile.html";
-}
-Array.prototype.slice.call(document.getElementsByClassName('login-goBack')).forEach(function(elem) {
-	elem.onclick = function() {
-		window.location = "index.html";
-	}
-});
+window.onload = function() {
+  document.getElementById("addFormApp").onsubmit = function(event) {
+    event.preventDefault();
+    var email = document.getElementById("app-email").value;
+    var password = document.getElementById("app-password").value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      firebase.auth().currentUser.
+      console.log(firebase.auth().currentUser);
+      window.location = "appProfile.html";
+    }, function(error) {
+      alert(error.message);
+    });
+  }
 
-$(document).ready(function() {
-  
-  // example event handler:
+  document.getElementById("addFormCom").onsubmit = function(event) {
+  	event.preventDefault();
+    var email = document.getElementById("comp-email").value;
+    var password = document.getElementById("comp-password").value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log(firebase.auth().currentUser);
+      window.location = "companyProfile.html";
+    }, function(error) {
+      alert(error.message);
+    });
+  }
+
+  for (elem of document.getElementsByClassName('login-goBack')) {
+    elem.onclick = function() {
+      window.location = "index.html";
+    }
+  }
+
   $(".login-loginInfo-C").hide();
-
-  $(".login-type-co1").click(function(){
-  		$(".login-loginInfo").hide();
-  		$(".login-loginInfo-C").show()
+  $(".login-type-co1").click(function() {
+    $(".login-loginInfo").hide();
+    $(".login-loginInfo-C").show();
   });
-
-  $(".login-type-ca2").click(function(){
-  		$(".login-loginInfo-C").hide();
-  		$(".login-loginInfo").show()
+  $(".login-type-ca2").click(function() {
+    $(".login-loginInfo-C").hide();
+    $(".login-loginInfo").show();
   });
-    
-});
+}
