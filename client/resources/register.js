7ae9f58a9d843f2f3bb5eac17bcfcb50681131a1
Raz Reed <raz@shipstone.com>
2016-12-08 23:21:33 -0500

    Add files via upload

--- client/resources/register.js
+++ client/resources/register.js
@@ -1,133 +1,94 @@
-$(document).ready(function() {
-  
-  // example event handler:
+window.onload = function() {
   $("#register-company").hide();
-
   $("#register-candidate").hide();
 
   $(".choice-block1").click(function(){
-  		$(".choice-centered").hide();
-      $("#register-login").hide();
-  		$("#register-candidate").show()
+		$(".choice-centered").hide();
+    $("#register-login").hide();
+    $("#register-candidate").show();
   });
 
   $(".choice-block2").click(function(){
-  		$(".choice-centered").hide();
-      $("#register-login").hide();
-  		$("#register-company").show()
+    $(".choice-centered").hide();
+    $("#register-login").hide();
+    $("#register-company").show();
   });
-    
-});
 
-document.getElementById('register-login').onclick = function(event) {
-  window.location = "login.html";
-}
-
-function applyID(){
-  event.preventDefault();
-  window.location = "applyID.html";
-}
-
-function validate1(formObj) {
-  var message = '';
-  var f = true;
-  if (formObj.firstName.value == "") {
-    message += "You must enter a first name\n";
-    formObj.firstName.focus();
-    f = false;
-  }
-
-  if (formObj.lastName.value == "") {
-    message += "You must enter a last name\n";
-    if (f == true) {
-      formObj.lastName.focus();
-      f = false;
+  document.getElementById("register-candidateForm").onsubmit = function(event) {
+    event.preventDefault();
+    var password = document.getElementById("appPassword").value;
+    var rePassword = document.getElementById("appRePassword").value;
+    if (password !== rePassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+    var firstName = document.getElementById("firstName").value;
+    var middleName = document.getElementById("middleName").value;
+    var lastName = document.getElementById("lastName").value;
+    if (middleName) {
+      var fullName = firstName + " " + middleName + " " + lastName;
+    }
+    else {
+      var fullName = firstName + " " + lastName;
+    }
+    var dob = document.getElementById("birthday").value;
+    var email = document.getElementById("appEmail").value;
+    if (firstName && lastName && dob && email && password) {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+          // push new user to applicants database
+          firebase.database().ref("users/applicants").push({
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "fullName": fullName,
+            "dob": dob,
+            "email": email
+          });
+          window.location = "appProfile.html";
+        }, function(error) {
+          alert(error.message);
+        });
+      }, function(error) {
+        alert(error.message);
+      });
+    }
+    else {
+      alert("Please enter all the required form elements.");
     }
   }
 
-  if (formObj.birthday.value == "") {
-    message += "You must enter a birthday\n";
-    if (f == true) {
-      formObj.birthday.focus();
-      f = false;
+  document.getElementById("register-companyForm").onsubmit = function(event) {
+    event.preventDefault();
+    var password = document.getElementById("companyPassword").value;
+    var rePassword = document.getElementById("companyRePassword").value;
+    if (password !== rePassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+    var id = document.getElementById("id").value;
+    var name = document.getElementById("companyName").value;
+    var email = document.getElementById("companyEmail").value;
+    if (id && name && email && password) {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+          // push new user to applicants database
+          firebase.database().ref("users/companies").push({
+            "id": id,
+            "name": name,
+            "email": email
+          });
+          window.location = "companyProfile.html";
+        }, function(error) {
+          alert(error.message);
+        });
+      }, function(error) {
+        alert(error.message);
+      });
     }
   }
 
-  if (formObj.email.value == "") {
-    message += "You must enter a email\n";
-    if (f == true) {
-      formObj.email.focus();
-      f = false;
-    }
+  document.getElementById('register-login').onclick = function(event) {
+    window.location = "login.html";
   }
-
-  if (formObj.password.value == "") {
-    message += "You must enter a password\n";
-    if (f == true) {
-      formObj.password.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.rePassword.value == "") {
-    message += "You must enter the password again\n";
-    if (f == true) {
-      formObj.rePassword.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.birthday.value != "" && formObj.rePassword.value != "" && formObj.password.value != "" && formObj.email.value != "" && formObj.lastName.value != "" && formObj.firstName.value != ""){
-    message += "Your form is successfully submitted"
-  }
-
-  alert(message)
-}
-
-function validate2(formObj) {
-  var message = '';
-  var f = true;
-  if (formObj.id.value == "") {
-    message += "You must enter an ID\n";
-    formObj.id.focus();
-    f = false;
-  }
-
-  if (formObj.companyName.value == "") {
-    message += "You must enter a company name\n";
-    if (f == true) {
-      formObj.companyName.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.email.value == "") {
-    message += "You must enter a email\n";
-    if (f == true) {
-      formObj.email.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.password.value == "") {
-    message += "You must enter a password\n";
-    if (f == true) {
-      formObj.password.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.rePassword.value == "") {
-    message += "You must enter the password again\n";
-    if (f == true) {
-      formObj.rePassword.focus();
-      f = false;
-    }
-  }
-
-  if (formObj.rePassword.value != "" && formObj.password.value != "" && formObj.email.value != "" && formObj.companyName.value != "" && formObj.id.value != ""){
-    message += "Your form is successfully submitted"
-  }
-
-  alert(message)
 }
